3025ccf89bae60d4f2a37a45879784a7eef14f22
Leul Teferi <leulteferi273@gmail.com>
2025-09-25 12:05:53 -0700

    feat: add YouTubeModule to LearningPlansModule imports for video integration

--- src/learning-plans/learning-plans.module.ts
+++ src/learning-plans/learning-plans.module.ts
@@ -3,9 +3,10 @@ import { LearningPlansService } from './learning-plans.service';
 import { LearningPlansController } from './learning-plans.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AiModule } from '../ai/ai.module';
+import { YouTubeModule } from '../youtube/youtube.module';
 
 @Module({
-  imports: [PrismaModule, AiModule],
+  imports: [PrismaModule, AiModule, YouTubeModule],
   controllers: [LearningPlansController],
   providers: [LearningPlansService],
   exports: [LearningPlansService],
